4cee84aada7482644f3c0eb0dcefee411c5572c4
Aromal <aromalsasidharan@gmail.com>
2017-11-01 16:10:27 +0530

    Sample Project added

---
+++ Pods/Target Support Files/Pods-SVProgressHUDAutoDismissTest/Pods-SVProgressHUDAutoDismissTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SVProgressHUDAutoDismissTest : NSObject
+@end
+@implementation PodsDummy_Pods_SVProgressHUDAutoDismissTest
+@end
---
+++ SVProgressHUDAutoDismissTest/SideMenuItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SideMenuItemViewController.swift
+//  SVProgressHUDAutoDismissTest
+//
+//  Created by Aromal Sasidharan on 11/1/17.
+//  Copyright © 2017 Aromal Sasidharan. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+class SideMenuItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SVProgressHUD.show()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
